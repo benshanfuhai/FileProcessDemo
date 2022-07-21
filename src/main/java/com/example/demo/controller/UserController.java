6c3041aa1a24172d90bb0903bba5514b443f1acc
benshanfuhai <2628254294@qq.com>
2022-07-21 15:44:14 +0800

    add file upload.

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,19 +1,47 @@
 package com.example.demo.controller;
 
+import com.example.demo.bean.StudentBean;
 import com.example.demo.service.UserService;
+import com.example.demo.utils.ExcelUtils;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-@RestController
+import java.util.List;
+
+@Controller
 public class UserController {
 
     @Autowired
     UserService userService;
 
+    @GetMapping("/hello")
+    // @ResponseBody
+    public String hello() {
+        return "manage";
+    }
+
     @PostMapping("/login")
+    @ResponseBody
     public boolean login(String username, String password) {
         return userService.verifyUserByUsername(username, password);
+    }
+
+    @PostMapping("file/upload")
+    @ResponseBody
+    public List<StudentBean> parseExcel(@RequestParam("file") MultipartFile multipartFile, Model model) throws Exception {
+        String fileName = multipartFile.getOriginalFilename();
+        // System.out.println(fileName);
+        if (!fileName.substring(fileName.length() - 5).equals(".xlsx")) {
+            model.addAttribute("error", "文件解析错误");
+            return null;
+        }
+        List<StudentBean> studentBeanList = ExcelUtils.excelToShopIdList(multipartFile.getInputStream());
+        model.addAttribute("msg", studentBeanList);
+        return studentBeanList;
     }
 
 }
