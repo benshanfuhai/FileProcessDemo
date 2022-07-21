5513944ee5eaa3a0af43c03a55b2cecfc05359d8
benshanfuhai <2628254294@qq.com>
2022-07-21 11:23:53 +0800

    update StudentBean.

--- src/main/java/com/example/demo/bean/StudentBean.java
+++ src/main/java/com/example/demo/bean/StudentBean.java
@@ -1,4 +1,93 @@
 package com.example.demo.bean;
 
 public class StudentBean {
+    private Integer id;
+    private String num;
+    private String name;
+    private Integer chineseScore;
+    private Integer mathScore;
+    private Integer englishScore;
+    private Integer totalScore;
+
+    public StudentBean(Integer id, String num, String name, Integer chineseScore, Integer mathScore, Integer englishScore, Integer totalScore) {
+        this.id = id;
+        this.num = num;
+        this.name = name;
+        this.chineseScore = chineseScore;
+        this.mathScore = mathScore;
+        this.englishScore = englishScore;
+        this.totalScore = totalScore;
+    }
+
+    public StudentBean() {
+    }
+
+    @Override
+    public String toString() {
+        return "StudentBean{" +
+                "id=" + id +
+                ", num='" + num + '\'' +
+                ", name='" + name + '\'' +
+                ", chineseScore=" + chineseScore +
+                ", mathScore=" + mathScore +
+                ", englishScore=" + englishScore +
+                ", totalScore=" + totalScore +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getChineseScore() {
+        return chineseScore;
+    }
+
+    public void setChineseScore(Integer chineseScore) {
+        this.chineseScore = chineseScore;
+    }
+
+    public Integer getMathScore() {
+        return mathScore;
+    }
+
+    public void setMathScore(Integer mathScore) {
+        this.mathScore = mathScore;
+    }
+
+    public Integer getEnglishScore() {
+        return englishScore;
+    }
+
+    public void setEnglishScore(Integer englishScore) {
+        this.englishScore = englishScore;
+    }
+
+    public Integer getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(Integer totalScore) {
+        this.totalScore = totalScore;
+    }
 }
